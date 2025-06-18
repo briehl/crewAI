88d900d73dbb5b6d85765cb9fcc44052382f1496
Bill Riehl <wjriehl@lbl.gov>
2025-06-18 15:20:11 -0400

    add api_key and base_url to internal instructor requests

--- src/crewai/utilities/internal_instructor.py
+++ src/crewai/utilities/internal_instructor.py
@@ -38,6 +38,6 @@ class InternalInstructor:
     def to_pydantic(self):
         messages = [{"role": "user", "content": self.content}]
         model = self._client.chat.completions.create(
-            model=self.llm.model, response_model=self.model, messages=messages
+            model=self.llm.model, response_model=self.model, messages=messages, base_url=self.llm.base_url, api_key=self.llm.api_key
         )
         return model
